dcc0f4be00385a419a6981ada92113fc088ef523
shobhit-varshney <108007491+shobhit-varshney@users.noreply.github.com>
2022-06-23 18:39:09 +0530

    UI layout changes (#8)
    * Update index.js
    
    * Update index.jsx
    
    * Update index.jsx
    
    * Update Nav.jsx
    
    * Update Nav.jsx
    
    * Update Graph.css
    
    * Update Gauge.js
    
    * Update SingleStat.js
    
    * Update index.jsx

--- src/Pages/Graph/SingleStat.js
+++ src/Pages/Graph/SingleStat.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function SingleStat(){
    return( <div style={{height:"250px", width:"400px",  textAlign: "center", paddingTop: "50px", marginLeft:"120px"}}>
+
         Output Rate
         <div style={{fontSize:"40px", paddingTop:"80px", fontWeight:"bold"}}>1,179 cph</div>
     </div>);
