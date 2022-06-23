da5f7a59f0a0732436fcf949db9295b7f255fea4
shobhit-varshney <108007491+shobhit-varshney@users.noreply.github.com>
2022-06-23 18:18:46 +0530

    Update SingleStat.js

--- src/Pages/Graph/SingleStat.js
+++ src/Pages/Graph/SingleStat.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function SingleStat(){
-   return( <div style={{height:"250px", width:"400px", backgroundColor:"black", color:"white", textAlign: "center"}}>
+   return( <div style={{height:"250px", width:"400px", backgroundColor:"black", color:"white", textAlign: "center", marginLeft:"70px"}}>
         Output Rate
         <div style={{fontSize:"40px", paddingTop:"80px", fontWeight:"bold"}}>1,179 cph</div>
     </div>);
